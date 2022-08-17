4f79fcc2c85bcb9aa87032ddd1a4581772d4b603
mja <michael@athanas.org>
2022-08-16 17:56:18 -0700

    created nodejs server with socketio

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const AmazonScraper = require('./Amazon');
 const { geo, defaultItemLimit } = require('./constant');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 
 const INIT_OPTIONS = {
     bulk: true,
@@ -92,3 +99,23 @@ exports.countries = async () => {
     }
     return output;
 };
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+    });
+
+io.on('connection', (socket) => {
+        console.log('a user connected');
+        socket.on('disconnect', () => {
+          console.log('user disconnected');
+        });
+        socket.on('chat message', (msg) => {
+            console.log('message: ' + msg);
+            io.emit('chat message', msg);
+          });
+      });
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+    });
